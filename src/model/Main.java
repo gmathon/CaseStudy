c48ca71c5e58d0b587a15687b7ffaee944781e02
gmathon <gmathon@nyit.edu>
2018-03-28 12:03:51 -0400

    Nw changes made

--- src/model/Main.java
+++ src/model/Main.java
@@ -8,9 +8,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-
-    //hi, testing
-
+//removed hi testing
         model.DataSource datasource = new DataSource();
         if (!datasource.open()) {
             System.out.println("Can't open datasource");
